4d5ceca3000665c3e747cf6382a395c0c7384598
Erica Basak <ericabasak@gmail.com>
2021-05-03 16:45:46 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,19 @@
+# run_code_inside = TRUE
+# puts "Code before if...end"
+# if run_code_inside
+#   puts "code inside"
+# puts "Code after if...end"
+
+
+# run_code_inside = true
+# puts "Code before 
+# if...end"
+# if run_code_inside
+#   puts "code inside"
+# end
+# puts "Code after 
+# if...end"
+
+
+run_code_inside
+puts "Code before if...end"
